cf51635723c738ceb09882ef0c63f10ef2209cb8
Ezequiel SM <seguramacedoezequiel@gmail.com>
2022-06-29 18:44:08 -0500

    Master

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template ("index.html")
 
+@app.route('/Proyecto1')
+def proyecto1():
+    return render_template ("proyecto1.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
